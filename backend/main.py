fb86c7009ca14567a50e606529e2cd1143bed6c7
Ali 🚶 <ali.aminfar@gmail.com>
2024-02-03 16:20:59 +1100

    setup log formatting for uvicorn

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
+import logging
 from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+logger = logging.getLogger("uvicorn")
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+
+for handler in logger.handlers:
+    handler.setFormatter(formatter)
 
 
 @app.get("/")
